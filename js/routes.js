fba97f4f437c4e90209001462707769fe54346ca
devpedro <contatopessoal879@gmail.com>
2023-06-21 13:13:08 -0300

    finalizado a barra de pesquisa

--- js/routes.js
+++ js/routes.js
@@ -1,17 +1,50 @@
-const links = document.getElementsByClassName("link");
-const pages = document.getElementsByClassName("page");
+function loginPage() {
+  document.getElementById("shopping").classList.add("hidden");
+  document.getElementById("search").classList.add("hidden");
+  document.getElementById("login").classList.remove("hidden");
+  document.getElementById("header_container").classList.add("hidden");
+  document.getElementById("foods_container").classList.add("hidden");
+  document.getElementById("about_container").classList.add("hidden");
+  document.getElementById("special_dishes_container").classList.add("hidden");
+  document.getElementById("special_scroll_id").classList.add("hidden");
+  document.getElementById("ads_container_div").classList.add("hidden");
+  document.getElementById("menu_container").classList.add("hidden");
+  document.getElementById("delivery_container").classList.add("hidden");
+  document.getElementById("blogs_container").classList.add("hidden");
+  document.getElementById("newsletter_container").classList.add("hidden");
+  document.getElementById("footer_container").classList.add("hidden");
+}
 
-for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function (event) {
-    event.preventDefault();
-    const targetPage = this.getAttribute("href").substring(1);
+function shoppingPage() {
+  document.getElementById("search").classList.add("hidden");
+  document.getElementById("login").classList.add("hidden");
+  document.getElementById("shopping").classList.remove("hidden");
+  document.getElementById("header_container").classList.add("hidden");
+  document.getElementById("foods_container").classList.add("hidden");
+  document.getElementById("about_container").classList.add("hidden");
+  document.getElementById("special_dishes_container").classList.add("hidden");
+  document.getElementById("special_scroll_id").classList.add("hidden");
+  document.getElementById("ads_container_div").classList.add("hidden");
+  document.getElementById("menu_container").classList.add("hidden");
+  document.getElementById("delivery_container").classList.add("hidden");
+  document.getElementById("blogs_container").classList.add("hidden");
+  document.getElementById("newsletter_container").classList.add("hidden");
+  document.getElementById("footer_container").classList.add("hidden");
+}
 
-    for (let j = 0; j < pages.length; j++) {
-      if (pages[j].id === targetPage) {
-        pages[j].classList.add("active");
-      } else {
-        pages[j].classList.remove("active");
-      }
-    }
-  });
+function searchPage() {
+  document.getElementById("shopping").classList.add("hidden");
+  document.getElementById("login").classList.add("hidden");
+  document.getElementById("search").classList.remove("hidden");
+  document.getElementById("header_container").classList.add("hidden");
+  document.getElementById("foods_container").classList.add("hidden");
+  document.getElementById("about_container").classList.add("hidden");
+  document.getElementById("special_dishes_container").classList.add("hidden");
+  document.getElementById("special_scroll_id").classList.add("hidden");
+  document.getElementById("ads_container_div").classList.add("hidden");
+  document.getElementById("menu_container").classList.add("hidden");
+  document.getElementById("delivery_container").classList.add("hidden");
+  document.getElementById("blogs_container").classList.add("hidden");
+  document.getElementById("newsletter_container").classList.add("hidden");
+  document.getElementById("footer_container").classList.add("hidden");
 }
