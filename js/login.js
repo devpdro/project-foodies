d6fa4e63d056643e088875d293d2a5ed5461a788
devpedro <contatopessoal879@gmail.com>
2023-06-26 17:31:31 -0300

    feat: barra de navegação nova

--- js/login.js
+++ js/login.js
@@ -2,15 +2,5 @@ function loginPage() {
   document.getElementById("shopping").classList.add("hidden");
   document.getElementById("search").classList.add("hidden");
   document.getElementById("login").classList.remove("hidden");
-  document.getElementById("header_container").classList.add("hidden");
-  document.getElementById("foods_container").classList.add("hidden");
-  document.getElementById("about_container").classList.add("hidden");
-  document.getElementById("special_dishes_container").classList.add("hidden");
-  document.getElementById("special_scroll_id").classList.add("hidden");
-  document.getElementById("menu_container").classList.add("hidden");
-  document.getElementById("delivery_container").classList.add("hidden");
-  document.getElementById("blogs_container").classList.add("hidden");
-  document.getElementById("newsletter_container").classList.add("hidden");
-  document.getElementById("footer_container").classList.add("hidden");
-  document.getElementById("ads_container_div_hidden").classList.add("hidden");
+  document.getElementById("hidden_container_all").classList.add("hidden");
 }
