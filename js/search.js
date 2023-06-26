65a20119e2ca6f75a35f69bce3eff8cfa3e51d43
devpedro <contatopessoal879@gmail.com>
2023-06-26 11:26:13 -0300

    feat: criação da responsividade para telas pequenas

---
+++ js/search.js
@@ -0,0 +1,16 @@
+function searchPage() {
+  document.getElementById("shopping").classList.add("hidden");
+  document.getElementById("login").classList.add("hidden");
+  document.getElementById("search").classList.remove("hidden");
+  document.getElementById("header_container").classList.add("hidden");
+  document.getElementById("foods_container").classList.add("hidden");
+  document.getElementById("about_container").classList.add("hidden");
+  document.getElementById("special_dishes_container").classList.add("hidden");
+  document.getElementById("special_scroll_id").classList.add("hidden");
+  document.getElementById("menu_container").classList.add("hidden");
+  document.getElementById("delivery_container").classList.add("hidden");
+  document.getElementById("blogs_container").classList.add("hidden");
+  document.getElementById("newsletter_container").classList.add("hidden");
+  document.getElementById("footer_container").classList.add("hidden");
+  document.getElementById("ads_container_div_hidden").classList.add("hidden");
+}
